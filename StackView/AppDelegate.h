821c6d66127b12cc2f6c0e69987dc118490c475f
Fushi yang <903696213@qq.com>
2023-06-25 15:32:24 +0800

    init project

---
+++ StackView/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  StackView
+//
+//  Created by Fushi yang on 2023/5/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ StackView/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  StackView
+//
+//  Created by Fushi yang on 2023/5/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
